ce27bd0989f91405ae4baeb3182c8eb943bb9e3f
Lelton Borges <lelton.borges1@gmail.com>
2020-02-29 23:31:31 -0300

    Projeto web services com Spring Boot e JPA / Hibernate

---
+++ src/test/java/com/estudo/courseSpring/CourseSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudo.courseSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
